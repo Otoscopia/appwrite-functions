efb4c2927a20e073f3600749e7340cf4678ff3ac
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-02-10 20:40:29 +0800

    ♻️ refactor (main): Update main.dart to send testing emails

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,47 @@
 import 'dart:async';
-import 'package:dart_appwrite/dart_appwrite.dart';
+import 'dart:io';
 
-// This is your Appwrite function
-// It's executed each time we get a request
+// import 'package:dart_appwrite/dart_appwrite.dart';
+import 'package:sendgrid_mailer/sendgrid_mailer.dart';
+
+final String? projectEndpoint = Platform.environment['APPWRITE_ENDPOINT'];
+final String? projectId = Platform.environment['APPWRITE_PROJECT_ID'];
+final String? appwriteApi = Platform.environment['APPWRITE_API_KEY'];
+final String? sendgridApiKey = Platform.environment['SENDGRID_API_KEY'];
+
 Future<dynamic> main(final context) async {
-// Why not try the Appwrite SDK?
-  //
-  // final client = Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(Platform.environment['APPWRITE_FUNCTION_PROJECT_ID'])
-  //    .setKey(Platform.environment['APPWRITE_API_KEY']);
-
-  // You can log messages to the console
-  context.log('Hello, Logs!');
-
-  // If something goes wrong, log an error
-  context.error('Hello, Errors!');
-
-  // The `req` object contains the request data
-  if (context.req.method == 'GET') {
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
-    return context.res.send('Hello, World!');
+  if (projectEndpoint == null ||
+      projectId == null ||
+      appwriteApi == null ||
+      sendgridApiKey == null) {
+    return context.res.json({
+      'error':
+          'Please set the APPWRITE_ENDPOINT, APPWRITE_FUNCTION_PROJECT_ID, APPWRITE_API_KEY and SENDGRID_API_KEY environment variables',
+    });
   }
 
-  // `res.json()` is a handy helper for sending JSON
-  return context.res.json({
-    'motto': 'Build like a team of hundreds_',
-    'learn': 'https://appwrite.io/docs',
-    'connect': 'https://appwrite.io/discord',
-    'getInspired': 'https://builtwith.appwrite.io',
+  // final client = Client()
+  //     .setEndpoint(projectEndpoint!)
+  //     .setProject(projectId)
+  //     .setKey(appwriteApi);
+
+  final mailer = Mailer(sendgridApiKey!);
+  final toAddress = Address('SEND TO EMAIL');
+  final fromAddress = Address('admin@otoscopia.me');
+  final content =
+      Content('text/plain', 'Appwrite Database successfully updated');
+  final subject = 'Database Update Alert!';
+  final personalization = Personalization([toAddress]);
+
+  final email =
+      Email([personalization], fromAddress, subject, content: [content]);
+  mailer.send(email).then((result) {
+    print(result);
+  }).catchError((onError) {
+    print('error');
+  });
+
+  context.res.json({
+    'data': "testing emails",
   });
 }
