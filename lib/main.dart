4429f28519ed720abacb89e10dac54e573915b0a
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-02-10 21:12:24 +0800

    ♻️ refactor: Remove nullable values from environment variable handling and email sending

--- lib/main.dart
+++ lib/main.dart
@@ -4,33 +4,23 @@ import 'dart:io';
 // import 'package:dart_appwrite/dart_appwrite.dart';
 import 'package:sendgrid_mailer/sendgrid_mailer.dart';
 
-final String? projectEndpoint = Platform.environment['APPWRITE_ENDPOINT'];
-final String? projectId = Platform.environment['APPWRITE_PROJECT_ID'];
-final String? appwriteApi = Platform.environment['APPWRITE_API_KEY'];
-final String? sendgridApiKey = Platform.environment['SENDGRID_API_KEY'];
-final String emailAddress = Platform.environment['EMAIL_ADDRESS']!; // ! for testing purposes only
+final String projectEndpoint = Platform.environment['APPWRITE_ENDPOINT']!;
+final String projectId = Platform.environment['APPWRITE_PROJECT_ID']!;
+final String appwriteApi = Platform.environment['APPWRITE_API_KEY']!;
+final String sendgridApiKey = Platform.environment['SENDGRID_API_KEY']!;
+final String emailAddress =
+    Platform.environment['EMAIL_ADDRESS']!; // ! for testing purposes only
 
 Future<dynamic> main(final context) async {
-  if (projectEndpoint == null ||
-      projectId == null ||
-      appwriteApi == null ||
-      sendgridApiKey == null) {
-    return context.res.json({
-      'error':
-          'Please set the APPWRITE_ENDPOINT, APPWRITE_FUNCTION_PROJECT_ID, APPWRITE_API_KEY and SENDGRID_API_KEY environment variables',
-    });
-  }
-
   context.log('Sending email to $emailAddress');
   context.log('Endpoint: $projectEndpoint, Project: $projectId, Key: $appwriteApi, Sendgrid Key: $sendgridApiKey');
-
 
   // final client = Client()
   //     .setEndpoint(projectEndpoint!)
   //     .setProject(projectId)
   //     .setKey(appwriteApi);
 
-  final mailer = Mailer(sendgridApiKey!);
+  final mailer = Mailer(sendgridApiKey);
   final toAddress = Address(emailAddress);
   final fromAddress = Address('admin@otoscopia.me');
   final content =
@@ -38,7 +28,8 @@ Future<dynamic> main(final context) async {
   final subject = 'Database Update Alert!';
   final personalization = Personalization([toAddress]);
 
-  context.log('Sending email to $emailAddress, from $fromAddress, with subject $subject and content $content');
+  context.log(
+      'Sending email to $emailAddress, from $fromAddress, with subject $subject and content $content');
 
   final email =
       Email([personalization], fromAddress, subject, content: [content]);
