fbaefa897dabb031756fa056213e071eeca9c325
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-02-11 20:07:32 +0800

    ✨ feat (constants): Update nurse email notification message

--- nurse-email-notification/lib/constants.dart
+++ nurse-email-notification/lib/constants.dart
@@ -12,14 +12,21 @@ const String kCode = "code";
 const String kScreening = "screening";
 const String kAssignment = "assignment";
 const String kUsers = "users";
+const String kName = "name";
 const String kEmail = "email";
 const String kType = "text/plain";
-String kContent(String code) => """
-A patient medical record has been modified, please check the patient's record for more information.
-\n 
-\n Regards, 
-\n Otoscopia Team. 
-\n https://otoscopia.vercel.app/
+String kContent(String name, String code) => """
+Dear Nurse $name,
+
+Your patient with the following code: $code and its medical record has been updated. Please log in to your Otoscopia account to view the patient's details along with the updated medical record. Please take necessary action if required and update the patients guardian about the status of the patient.
+
+Thank you and we look forward to the successful treatment of the patient.
+
+Sincerely,
+
+Otoscopia Team
+
+This communication is intended solely for the use of the addressee. It may contain confidential or legally privileged information. If you are not the intended recipient, any disclosure, copying, distribution or taking any action in reliance on this communication is strictly prohibited and may be unlawful. If you received this communication in error, please notify the sender immediately and delete this communication from your system. Otoscopia is neither liable for the proper and complete transmission of this communication nor for any delay in its receipt.
 """;
 
 const String kSubject = "New Patient has been added.";
