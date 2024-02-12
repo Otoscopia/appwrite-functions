2096e41e544d8695ae612da1822bcb42e94ba8b7
Laurence Troy Sambaa Valdez <laurencetroyv@gmail.com>
2024-02-12 19:17:12 +0800

    🚀 deploy: Add account validation notification funcationality

--- account-validation-notification/lib/constants.dart
+++ account-validation-notification/lib/constants.dart
@@ -1,13 +1,3 @@
-// Appwrite Client environment variables
-const String kEndpoint = "APPWRITE_ENDPOINT";
-const String kProjectID = "APPWRITE_PROJECT_ID";
-const String kAppwriteAPI = "APPWRITE_API_KEY";
-
-// Appwrite Database environment variables
-const String kDatabaseID = "DATABASE_ID";
-const String kUsersCollection = "USERS_COLLECTION";
-const String kAssignmentCollection = "ASSIGNMENT_COLLECTION";
-
 // Sendgrid environment variables
 const String kSendgridAPI = "SENDGRID_API_KEY";
 const String kEmailAddress = "EMAIL_ADDRESS";
@@ -15,14 +5,12 @@ const String kContactEmail = "CONTACT_EMAIL";
 
 // Constants
 const String kType = "text/plain";
-String kContent(String name, String email) => """
+String kContent(String name) => """
 Dear ${name.toUpperCase()},
 
-Thank you for creating your User Profile! You are halfway in opening your Otoscopia account with Otoscopia Team.
+Your Otoscopia User Profile haas been verified!
 
-We are excited to have you on board and we are looking forward to working with you. We will be in touch with you soon to discuss the next steps. In the meantime, if you have any questions, please feel free to reach out to us at $email.
-
-Thank you and we look forward to welcoming you to the Otoscopia family.
+We are looking forward to working with you. You may access your account via the Web, Desktop or Mobile Application.
 
 Sincerely,
 
@@ -31,33 +19,25 @@ Otoscopia Team
 This communication is intended solely for the use of the addressee. It may contain confidential or legally privileged information. If you are not the intended recipient, any disclosure, copying, distribution or taking any action in reliance on this communication is strictly prohibited and may be unlawful. If you received this communication in error, please notify the sender immediately and delete this communication from your system. Otoscopia is neither liable for the proper and complete transmission of this communication nor for any delay in its receipt.
 """;
 
-const String kSubject = "Otoscopia User Profile Created Successfully";
+const String kSubject = "Otoscopia User Profile Verified";
+
+const kEmailVerification = "emailVerification";
+const kphoneVerification = "phoneVerification";
+const kLabels = "labels";
+const kDoctor = "doctor";
+const kNurse = "nurse";
 
 const String kData = "data";
 const String kSuccess = "Email has been Sent Successfully.";
 
-const String kID = "userId";
 const String kName = "name";
-const String kRole = "role";
 const String kEmail = "email";
-const String kPhone = "phone";
-const String kPublicKey = "publicKey";
-const String kWorkAddress = "workAddress";
-const String kSchools = "schools";
-const String kIsActive = "isActive";
-const String kUsers = "users";
 
 const String kError = "Failed to create document";
 
 // logs
-const String kSettingUpAppwriteClient = "Setting up Appwrite client...";
-const String kSettingUpAppwriteDatabase = "Setting up Appwrite database...";
-const String kSettingUpAppwriteUsers = "Setting up Appwrite users...";
 const String kSettingUpSendgridMailer = "Setting up Sendgrid mailer...";
 const String kDecodingRequestBody = "Decoding request body...";
-const String kCreatingUser = "Creating user...";
-const String kUpdatingUser = "Updating user...";
-const String kCreatingAssignment = "Creating assignment...";
 const String kSettingUpEmail = "Setting up email content and subject...";
 const String kSendingEmail = "Sending email...";
 const String kEmailSent = "Email has been sent successfully.";
--- account-validation-notification/lib/main.dart
+++ account-validation-notification/lib/main.dart
@@ -1,110 +1,54 @@
 import 'dart:async';
+import 'dart:convert';
 // import 'dart:convert';
 import 'dart:io';
 
-// import 'package:dart_appwrite/dart_appwrite.dart';
-// import 'package:sendgrid_mailer/sendgrid_mailer.dart';
+import 'package:sendgrid_mailer/sendgrid_mailer.dart';
 import 'package:starter_template/constants.dart';
-
-// Appwrite Client environment variables
-final String projectEndpoint = Platform.environment[kEndpoint]!;
-final String projectID = Platform.environment[kProjectID]!;
-final String appwriteApi = Platform.environment[kAppwriteAPI]!;
-
-// Appwrite Database environment variables
-final String databaseID = Platform.environment[kDatabaseID]!;
-final String usersCollection = Platform.environment[kUsersCollection]!;
-final String assignmentCollection =
-    Platform.environment[kAssignmentCollection]!;
 
 // Sendgrid environment variables
 final String sendgridAPI = Platform.environment[kSendgridAPI]!;
 final String emailAddress = Platform.environment[kEmailAddress]!;
-final String contactEmail = Platform.environment[kContactEmail]!;
 
 Future<dynamic> main(final context) async {
-  // context.log(kSettingUpAppwriteClient);
-  // final client = Client()
-  //     .setEndpoint(projectEndpoint)
-  //     .setProject(projectID)
-  //     .setKey(appwriteApi);
+  context.log(kSettingUpSendgridMailer);
+  final mailer = Mailer(sendgridAPI);
 
-  // context.log(kSettingUpAppwriteDatabase);
-  // final databases = Databases(client);
+  final fromAddress = Address(emailAddress);
+  late final Content content;
+  final subject = kSubject;
 
-  // context.log(kSettingUpAppwriteUsers);
-  // final user = Users(client);
-
-  // context.log(kSettingUpSendgridMailer);
-  // final mailer = Mailer(sendgridAPI);
-
-  // final fromAddress = Address(emailAddress);
-  // late final Content content;
-  // final subject = kSubject;
-
-  // late final Address toAddress;
-  // late final Personalization personalization;
-  // late final Email email;
+  late final Address toAddress;
+  late final Personalization personalization;
+  late final Email email;
 
   try {
-    // context.log(kDecodingRequestBody);
-    context.log(context.req.bodyRaw);
-    // final body = json.decode(context.req.bodyRaw);
-    // context.log(body);
+    context.log(kDecodingRequestBody);
+    final body = json.decode(context.req.bodyRaw);
 
-    // final userID = body[kID];
+    final emailVerified = body[kEmailVerification];
+    final phoneVerified = body[kphoneVerification];
+    final List<String> labels = List<String>.from(body[kLabels]);
 
-    // context.log(kCreatingUser);
-    // await databases.createDocument(
-    //   databaseId: databaseID,
-    //   collectionId: usersCollection,
-    //   documentId: userID,
-    //   data: {
-    //     kName: body[kName],
-    //     kRole: body[kRole],
-    //     kEmail: body[kEmail],
-    //     kPhone: body[kPhone],
-    //     kPublicKey: body[kPublicKey],
-    //     kWorkAddress: body[kWorkAddress],
-    //   },
-    //   permissions: [
-    //     Permission.update(Role.user(userID)),
-    //   ],
-    // );
+    if (emailVerified && phoneVerified && labels.contains(kDoctor) ||
+        labels.contains(kNurse)) {
+      context.log(kSettingUpEmail);
+      content = Content(kType, kContent(body[kName]));
+      toAddress = Address(body[kEmail]);
+      personalization = Personalization([toAddress]);
 
-    // context.log(kUpdatingUser);
-    // await user.updatePhone(userId: userID, number: body[kPhone]);
-    // // await user.updateLabels(
-    // // userId: '[USER_ID]',
-    // // labels: [],
-    // // );
+      email =
+          Email([personalization], fromAddress, subject, content: [content]);
 
-    // context.log(kCreatingAssignment);
-    // await databases.createDocument(
-    //   databaseId: databaseID,
-    //   collectionId: assignmentCollection,
-    //   documentId: ID.unique(),
-    //   data: {
-    //     kIsActive: true,
-    //     kUsers: userID,
-    //     kSchools: body[kSchools],
-    //   },
-    // );
+      context.log(kSendingEmail);
+      await mailer.send(email);
 
-    // context.log(kSettingUpEmail);
-    // content = Content(kType, kContent(body[kName], contactEmail));
-    // toAddress = Address(body[kEmail]);
-    // personalization = Personalization([toAddress]);
+      context.log(kEmailSent);
 
-    // email = Email([personalization], fromAddress, subject, content: [content]);
-
-    // context.log(kSendingEmail);
-    // await mailer.send(email);
-
-    // context.log(kEmailSent);
-    return context.res.json({
-      kData: kSuccess,
-    });
+      return context.res.json({
+        kData: kSuccess,
+      });
+    }
   } catch (e) {
     context.error("$kError: $e");
     return context.res.send(kError);
